9a444a2b794a999e8eb58e424af8501a10ce5c45
Muthupandian <muthupandiansuresh2003@gmail.com>
2025-12-25 19:15:11 +0530

    new files added

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
 from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)-> List[str]:
+    with open('requirements.txt', 'r') as file:
+        requirement=file.readlines()
+    requirement=[req.strip() for req in requirement if req.strip()!= '-e .']
+    return requirement
+
+setup(
+    name='MLOPS',
+    version='0.0.1',
+    author='Muthupandian',
+    author_email='muthupandiansuresh2003@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
