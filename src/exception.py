d0110346aae6b76c8fb5c2b21fe26ab3d386fbc9
Muthupandian <muthupandiansuresh2003@gmail.com>
2025-12-30 12:36:45 +0530

    logger created and coded

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,11 @@
 import sys
+from logger import logging
+
 
 def get_error_message(error,error_details:sys):
     _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message=(f'error occured in python script [{file_name}] in line no [{exc_tb.tb_lineno}] error message [{str(error)}]')
+    error_message=(f'Error occured in python script [{file_name}] in line no [{exc_tb.tb_lineno}] error message [{str(error)}]')
     return error_message
 
 class CustomeException(Exception):
@@ -13,3 +15,11 @@ class CustomeException(Exception):
 
     def __str__(self):
         return self.error_message
+
+
+if __name__=='__main__':
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info('zero error')
+        raise CustomeException(e,sys)
--- src/logger.py
+++ src/logger.py
@@ -1,2 +1,16 @@
-import sys
 import logging
+import os
+from datetime import datetime
+
+log_path=os.path.join(os.getcwd(),'logs',f'{datetime.now().strftime('%d_%m_%Y')}') #joining the path D:\Mlops\logs\12_30_2025 
+LOG_FILE=f'{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log'
+os.makedirs(log_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format='[%(asctime)s] line: %(lineno)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
+
