a38afd0b13ab32e283d2d939abb04c41aa8ed13d
Muthupandian <muthupandiansuresh2003@gmail.com>
2026-01-04 21:09:23 +0530

    predictpipeline were created

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ app=Flask(__name__)
 @app.route("/home")
 def welcome():
     return render_template('index.html')
+
+@app.route('/prediction', methods=['GET','POST'])
+def prediction():
+    if request.method == 'GET':
+        return render_template('home.html')
+    
